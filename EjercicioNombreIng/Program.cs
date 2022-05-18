22b3cedbbd84f1b2b5cfa1051b2bf4cff19359f7
MarianoGomez04 <mgomezdisalvo@gmail.com>
2022-05-18 12:19:55 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioNombreIng/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+// Pedir nombre//
+Console.WriteLine("Hola! Ingresa un nombre por favor:");
+//Variables//
+var NombreIngresado = Console.ReadLine();
+
+//Saludar//
+Console.WriteLine($"Hola {NombreIngresado}!");
+
+//Preguntar si quiere continuar//
+Console.WriteLine("Quiere continuar? S/N");
+//variable opcion//
+var opcion = Console.ReadLine();
+
+
+while (opcion.ToUpper() == "S")
+{
+    //Borrar lineas//
+    Console.Clear();
+
+    //Pedir nombre//
+    Console.Write("Hola! Ingresa un nombre por favor:");
+    NombreIngresado = Console.ReadLine();
+
+    //Saludar//
+    Console.WriteLine($"Hola {NombreIngresado}!");
+
+    Console.WriteLine("Quiere continuar? S/N");
+
+    opcion = Console.ReadLine();
+}
+
+if (opcion.ToUpper() == "N")
+{
+    Console.WriteLine("El programa ha finalizado.");
+
+}
+else
+{
+    Console.WriteLine("Opcion ingresada no valida. El programa ha finalizado.");
+}
